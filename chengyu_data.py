811702758ecefcad536ded138c18969c9243ae4d
Michael Simo <michael@mpmsimo.com>
2016-03-24 00:25:23 -0400

    Update print format
    Changed data schema, and updated how data is printed. Not tested because
    windoze :(

--- chengyu_data.py
+++ chengyu_data.py
@@ -6,28 +6,27 @@
 
 import json
 
-output_header = ("{0} | {1}")
-output_format = ("""{0}:
-{1}:
-{2}:""")
+output_format = ("""{0} | {1}
+{2}: {5}
+{3}: {6}
+{4}: {7}""") #Chengyu, Pinyin 
 
-en_format = output_format.format("Chinese meaning", "English meaning", "English equivalent")
-zh_cn_format = output_format.format("placeholder", "placeholder", "placeholder")
+en_format = ["Chinese meaning", "English meaning", "English equivalent"]
+zh_cn_format = ["中文意思", "英文意思", "英文等同于"]
 
-'''
-json_schema = {"id": {
-				"chengyu": chengyu,
-				"pinyin": pinyin,
-				"zh_cn_meaning": zh_cn_translation,
-				"en_meaning": en_translation,
-				"en_equivalent": en_equivalent }
-			}
-'''
-
-chengyu = {"id": {
+chengyu = {1: {
 		"chengyu": "通权达变",
 		"pinyin": "tōng quán dá biàn",
 		"zh_cn_meaning": "To be capable of versatility",
 		"en_meaning": "To adapt to any situation",
-		"en_equivalent": "placeholder"
-}
+		"en_equivalent": "placeholder"},
+		"chengyu": "一窍不通",
+		"pinyin": "yī qiào bù tōng",
+		"zh_cn_meaning": "I don't understand a word",
+		"en_meaning": "It's all Greek to me",
+		"en_equivalent": "placeholder"}}
+
+def get_list_length():
+	"""Gets the length of the dictionary."""
+	list_length = len(chengyu.keys())
+	return list_length
--- hello_chengyu.py
+++ hello_chengyu.py
@@ -4,29 +4,38 @@
 #Author: mpmsimo
 #Date Created: 12/10/15
 
+import random imort randint
 import json
+
 import chengyu_data as cd
 
-def select_language(language = 'en'):
+def select_language(language = None):
 	if language != 'en' or language != 'zh_cn':
 		print("Please select 'en' (English) or 'zh_cn' (Simplified Chinese).")
 	return language
 
-def format_output(language):
-	if language == 'en':
-		return cd.en_format
-	elif language == 'zh_cn':
-		return cd.zh_cn_format
-
-def print_chengyu(chengyu_data):
+def print_chengyu(language, chengyu_id):
 	"""Prints a chengyu or Chinese proverb alongside the English translation and meaning."""
 	pass
+	if language == 'en':
+		language_format = cd.en_format
+	elif language == 'zh_cn':
+		language_format = cd.zh_cn_format
+	output_format.format(chengyu_id["chengyu"], chengyu_id["pinyin"],
+						language_format[0], language_format[1],
+						language_format[2], chengyu_id["zh_cn_meaning"],
+						chengyu_id["en_meaning"], chengyu_id["en_equivalent"])
+	print(output_format)
+
+def randomize():
+	"""Generate a random number associated with a chengyu to print to terminal."""
+	chengyu_num = randint(0, cd.get_list_length())
+	return chengyu_num
 
 def main():
 	language = select_language()
-	output = format_output(language)
-	print_chengyu(chengyu_data)
+	chengyu_num = randomize()
+	print_chengyu(language, chengyu_num)
 	
-
 if __name__ == '__main__':
 	main()
