e2d77c9fe6a0b4d47854c47d4029b6058743ff69
Michael Simo <os@mpmsimo.com>
2016-07-06 01:20:54 -0400

    Prints output of random chengyu

--- chengyu_data.py
+++ chengyu_data.py
@@ -6,20 +6,16 @@
 
 import json
 
-output_format = ("""{0} | {1}
-{2}: {5}
-{3}: {6}
-{4}: {7}""") #Chengyu, Pinyin 
-
 en_format = ["Chinese meaning", "English meaning", "English equivalent"]
 zh_cn_format = ["中文意思", "英文意思", "英文等同于"]
 
-chengyu = {1: {
+chengyu = {'1': {
 		"chengyu": "通权达变",
 		"pinyin": "tōng quán dá biàn",
 		"zh_cn_meaning": "To be capable of versatility",
 		"en_meaning": "To adapt to any situation",
 		"en_equivalent": "placeholder"},
+        '2': {
 		"chengyu": "一窍不通",
 		"pinyin": "yī qiào bù tōng",
 		"zh_cn_meaning": "I don't understand a word",
--- hello_chengyu.py
+++ hello_chengyu.py
@@ -4,38 +4,32 @@
 #Author: mpmsimo
 #Date Created: 12/10/15
 
-import random imort randint
+from random import randint
 import json
 
 import chengyu_data as cd
 
-def select_language(language = None):
-	if language != 'en' or language != 'zh_cn':
-		print("Please select 'en' (English) or 'zh_cn' (Simplified Chinese).")
-	return language
-
 def print_chengyu(language, chengyu_id):
-	"""Prints a chengyu or Chinese proverb alongside the English translation and meaning."""
-	pass
-	if language == 'en':
-		language_format = cd.en_format
-	elif language == 'zh_cn':
-		language_format = cd.zh_cn_format
-	output_format.format(chengyu_id["chengyu"], chengyu_id["pinyin"],
-						language_format[0], language_format[1],
-						language_format[2], chengyu_id["zh_cn_meaning"],
-						chengyu_id["en_meaning"], chengyu_id["en_equivalent"])
-	print(output_format)
+    """Prints a chengyu or Chinese proverb alongside the English translation and meaning."""
+    if language == 'zh_cn':
+        language_format = cd.zh_cn_format
+    else:
+        language_format = cd.en_format
+
+    jd = json.dumps(cd.chengyu, ensure_ascii=False)
+    jl = json.loads(jd)
+    output = jl.get(chengyu_id)
+    for k, v in output.items():
+        print(u'{0}: {1}'.format(k, v))
 
 def randomize():
 	"""Generate a random number associated with a chengyu to print to terminal."""
-	chengyu_num = randint(0, cd.get_list_length())
-	return chengyu_num
+	chengyu_num = randint(1, cd.get_list_length())
+	return str(chengyu_num)
 
 def main():
-	language = select_language()
-	chengyu_num = randomize()
-	print_chengyu(language, chengyu_num)
-	
+    chengyu_num = randomize()
+    print_chengyu('en', chengyu_num)
+
 if __name__ == '__main__':
 	main()
